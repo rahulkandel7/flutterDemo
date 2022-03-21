e4337d4e9787bd6b30d176af558f153c7d99b229
Rahul Sharma <rahulkandel7@gmail.com>
2022-03-21 22:27:46 +0545

    Homepage completed

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -14,91 +14,343 @@ class Homepage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 15.0,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.menu,
-                      color: Colors.black,
-                    ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                          10,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.menu,
+                        color: Colors.black,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          print('welcome cart');
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              10,
+                            ),
+                          ),
+                          color: Color.fromRGBO(
+                            158,
+                            126,
+                            237,
+                            1,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Icon(
+                              Icons.shopping_bag,
+                              color: Colors.white,
+                              size: 18,
+                            ),
+                          ),
                         ),
                       ),
-                      color: Color.fromRGBO(
-                        158,
-                        126,
-                        237,
-                        1,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Icon(
-                          Icons.shopping_bag,
-                          color: Colors.white,
-                          size: 18,
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  children: [
-                    Text(
-                      'Fresh ',
-                      style: TextStyle(
-                        color: Colors.indigo,
-                        fontSize: 34,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'fruits and vegetables',
-                      style: TextStyle(
-                        fontSize: 34,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 50,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, i) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0,
-                        vertical: 5.0,
-                      ),
-                      child: Text(
-                        options[i],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Fresh ',
                         style: TextStyle(
+                          color: Colors.indigo,
+                          fontSize: 34,
                           fontWeight: FontWeight.bold,
-                          fontSize: 22,
-                          color: Colors.indigo[400],
                         ),
                       ),
-                    );
-                  },
-                  itemCount: options.length,
+                      Text(
+                        'fruits and vegetables',
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, i) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0,
+                          vertical: 5.0,
+                        ),
+                        child: Text(
+                          options[i],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                            color: Colors.indigo[400],
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: options.length,
+                  ),
+                ),
+                SizedBox(
+                  height: 613,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 500,
+                        margin: EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(
+                              50,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 5.0,
+                                color: Colors.black12,
+                                offset: Offset(3, 1),
+                              )
+                            ]),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 500,
+                              width: double.infinity,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(
+                                    50,
+                                  ),
+                                  topRight: Radius.circular(
+                                    50,
+                                  ),
+                                ),
+                                child: Image.network(
+                                  'https://thumbs.dreamstime.com/b/fashionable-trendy-pineapple-fruit-headphones-sun-glasses-listen-to-music-over-bright-pastel-yellow-background-134571779.jpg',
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 40.0,
+                                vertical: 20.0,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Pineapple',
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Rs. 45",
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: 5,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20.0,
+                  ),
+                  child: Text(
+                    'Pre-Order',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo.shade700,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(
+                    20,
+                  ),
+                  padding: EdgeInsets.all(
+                    20,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.0,
+                      color: Colors.black12,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          color: Colors.black12,
+                          offset: Offset(3, 2)),
+                    ],
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(
+                                30,
+                              ),
+                              child: Image.network(
+                                'https://health.clevelandclinic.org/wp-content/uploads/sites/3/2019/06/cropped-GettyImages-643764514.jpg',
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 28.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Boom Mixes',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Text(
+                                    'Boom Mixes',
+                                    style: TextStyle(
+                                      fontSize: 26,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w100,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'Rs 250',
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.indigo.shade800,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(
+                    20,
+                  ),
+                  padding: EdgeInsets.all(
+                    20,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1.0,
+                      color: Colors.black12,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 5.0,
+                          color: Colors.black12,
+                          offset: Offset(3, 2)),
+                    ],
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(
+                                30,
+                              ),
+                              child: Image.network(
+                                'https://thumbs.dreamstime.com/b/heart-shape-various-vegetables-fruits-healthy-food-concept-isolated-white-background-140287808.jpg',
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 28.0),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Boom Mixes',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Text(
+                                    'Boom Mixes',
+                                    style: TextStyle(
+                                      fontSize: 26,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w100,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'Rs 250',
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.indigo.shade800,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
